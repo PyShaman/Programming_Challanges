6b1c0e7c67b052006547630d448657f14e097096
Michał <michal.bek@gmail.com>
2017-01-10 17:35:50 +0100

    Add files via upload

---
+++ 16 Reverse a string/16 Reverse a string.py
@@ -0,0 +1,8 @@
+# Task 16 Reverse a string from Pro/g/ramming Challanges [Made by Anonymous in January 2015]
+
+
+def string_converter(string):
+	return string[::-1]
+	
+new_string = input("Write a string that will be reversed: ")
+print("Reversed string is:", string_converter(new_string))
